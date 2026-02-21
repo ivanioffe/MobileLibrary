43f8189b88dc9e759b23cd5e30129fefad35aa93
Ivan Ioffe <iiooffee0202@gmail.com>
2026-02-21 12:24:58 +0300

    feat(datastore-auth): add :core:datastore-auth module

---
+++ core/datastore-auth/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.ioffeivan.android.library)
+    alias(libs.plugins.ioffeivan.hilt)
+}
+
+android {
+    namespace = "com.ioffeivan.core.datastore_auth"
+
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+}
+
+dependencies {
+}
