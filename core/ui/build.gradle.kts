d9519f8cdcd48ff903de7dfe97f0d1462638e3e2
Ivan Ioffe <iiooffee0202@gmail.com>
2026-02-22 12:29:04 +0300

    feat(ui): add :core:ui module

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.ioffeivan.android.library)
+}
+
+android {
+    namespace = "com.ioffeivan.core.ui"
+
+    defaultConfig {
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
