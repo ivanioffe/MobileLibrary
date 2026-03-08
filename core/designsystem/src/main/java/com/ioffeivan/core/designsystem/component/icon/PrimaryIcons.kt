656dc061c2edf57957def37e8f6f68afb17d004d
Ivan Ioffe <iiooffee0202@gmail.com>
2026-03-08 21:39:35 +0300

    feat(app): update app icon and splash screen

--- core/designsystem/src/main/java/com/ioffeivan/core/designsystem/component/icon/PrimaryIcons.kt
+++ core/designsystem/src/main/java/com/ioffeivan/core/designsystem/component/icon/PrimaryIcons.kt
@@ -5,8 +5,8 @@ import com.ioffeivan.core.designsystem.R
 object PrimaryIcons {
     val ArrowBack = R.drawable.ic_arrow_back
     val Close = R.drawable.ic_close
-    val FilledFavourite = R.drawable.filled_favourite
+    val FilledFavourite = R.drawable.ic_filled_favourite
     val Google = R.drawable.ic_google
-    val OutlinedFavourite = R.drawable.outlined_favourite
+    val OutlinedFavourite = R.drawable.ic_outlined_favourite
     val Search = R.drawable.ic_search
 }
